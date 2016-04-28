020c45d83149618d9a42b1dc5ccbca62ce76bfd2
Brad Hughes <bradleythughes@gmail.com>
2016-04-28 13:55:40 -0400

    change titles

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,5 @@
 class StaticPagesController < ApplicationController
+
   def home
     redirect_to brightsides_path if signed_in?
   end
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
   resources :shines
 
   root to: 'static_pages#home'
+
   match '/about', to: 'static_pages#about', via: 'get'
 
 end
